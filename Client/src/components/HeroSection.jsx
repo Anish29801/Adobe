09ddc43ca50264d666364cb0df1e1380a877168e
Anish Agrawal <103357921+Anish29801@users.noreply.github.com>
2024-10-22 11:40:03 +0530

    Hero Commit 1

---
+++ Client/src/components/HeroSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HeroSection = () => {
+    return (
+        <div className="hero_1">
+            <div className="bg-gradient-to-r from-purple-200 via-pink-300 to-blue-200 min-h-screen flex items-center justify-center">
+                <div className="text-center p-8">
+                    <h2 className="text-5xl font-semibold mb-4">Which superpower will it be?</h2>
+                    <h4 className="text-xl">Free creative fun or premium possibilities?</h4>
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HeroSection;
