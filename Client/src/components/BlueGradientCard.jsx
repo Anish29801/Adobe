627e7dcd550d640707707a121e33eee3d6b578df
Anish Agrawal <103357921+Anish29801@users.noreply.github.com>
2024-10-23 17:39:14 +0530

    Card Set1 Commit

--- Client/src/components/BlueGradientCard.jsx
+++ Client/src/components/BlueGradientCard.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import main2 from '../assets/main2.jpg';
+import Adobe from "../assets/Adobe.svg";
 
 const BlueGradientCard = () => {
   return (
-    <div className="p-6 rounded-lg bg-gradient-to-r from-blue-500 to-blue-700 text-white shadow-md">
-      <h2 className="text-2xl font-bold mb-4">34% off the first year on Creative Cloud All Cards</h2>
-      <p className="mb-4">
-        Get up to 9 licences for your team at a lower price.
+    <div className="p-6 rounded-lg bg-gradient-to-r from-blue-500 to-blue-700 shadow-md">
+      <h2 className="font-semibold">BUSINESS</h2>
+      <h2 className="text-2xl font-bold mb-4">
+        34% off the first year on Creative Cloud All Cards
+      </h2>
+      <p className="mb-4 text-md">
+        Get up to 9 licenses for your team at a lower price.*
       </p>
+      <a href="#" className='underline'>
+        <h2>Learn more</h2>
+      </a>
+      <div className="relative mt-8">
+        <img
+          src={main2}
+          alt="Card main 2"
+          className="w-full h-48 sm:h-64 md:h-80 lg:h-96 object-cover rounded-lg"
+        />
+        <img src={Adobe} alt="Adobe logo" className="absolute bottom-4 right-4 w-4" />
+      </div>
     </div>
   );
 };
--- Client/src/components/PinkPurpleGradientCard.jsx
+++ Client/src/components/PinkPurpleGradientCard.jsx
@@ -4,9 +4,40 @@ const PinkPurpleGradientCard = () => {
   return (
     <div className="p-6 rounded-lg bg-gradient-to-r from-[#f05a9b] to-[#834dff] text-white shadow-md">
       <h2 className="text-2xl font-bold mb-4">Fast and Free Editing with Adobe Express</h2>
-      <p className="mb-4">
-        Get up to 9 licences for your team at a lower price.
-      </p>
+
+      <div className="bg-[#ededed] -m-6 mt-4 p-4 text-black rounded-b-lg">
+        <h2 className="text-xl font-semibold mb-2">Simple image editing:</h2>
+        <ul className="list-none mb-4">
+          <li className="mb-1">
+            <a href="#" className="font-md hover:underline ">Remove Background</a>
+          </li>
+          <li className="mb-1">
+            <a href="#" className="font-md hover:underline ">Resize Images</a>
+          </li>
+          <li className="mb-1">
+            <a href="#" className="font-md hover:underline ">Crop Images</a>
+          </li>
+          <li className="mb-1">
+            <a href="#" className="font-md hover:underline ">Convert to SVG</a>
+          </li>
+        </ul>
+
+        <h2 className="text-xl font-semibold mb-2">Quick video editing</h2>
+        <ul className="list-none">
+          <li className="mb-1">
+            <a href="#" className="font-md hover:underline ">Trim Videos</a>
+          </li>
+          <li className="mb-1">
+            <a href="#" className="font-md hover:underline ">Merge Videos</a>
+          </li>
+          <li className="mb-1">
+            <a href="#" className="font-md hover:underline ">Crop Videos</a>
+          </li>
+          <li className="mb-1">
+            <a href="#" className="font-md hover:underline ">Convert to GIF</a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
