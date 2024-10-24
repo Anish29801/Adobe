c185b326887593595364644e8ba88a18dada67f2
Anish Agrawal <103357921+Anish29801@users.noreply.github.com>
2024-10-24 17:54:21 +0530

    Navbar Commit

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,12 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Adobe from "../assets/Adobe.svg";
+import AdobeExpressIcon from '../assets/adobe-express.webp';
+import AcrobatIcon from '../assets/acrobat.svg';
+import PhotoshopIcon from '../assets/photoshop.svg';
+import LightroomIcon from '../assets/lightroom.svg';
+import AcrobatSignIcon from '../assets/acrobat-sign.webp';
+import FontsIcon from '../assets/fonts.webp';
+import StockIcon from '../assets/stock.webp';
+import ExperienceCloudIcon from '../assets/experience-cloud.webp';
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+    const handleClickOutside = (event) => {
+        if (!event.target.closest('.dropdown-container') && isDropdownOpen) {
+            setIsDropdownOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('click', handleClickOutside);
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [isDropdownOpen]);
 
     return (
         <nav className="bg-white sticky top-0 z-50 shadow-md">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative">
                 <div className="flex justify-between h-16">
                     <div className="flex">
                         <div className="flex-shrink-0 flex items-center">
@@ -15,58 +41,72 @@ function Navbar() {
                                 <h2 className="text-2xl font-bold text-red-600">Adobe</h2>
                             </a>
                         </div>
-                        {/* Desktop Menu */}
                         <div className="hidden md:ml-6 md:flex space-x-4">
-                            <a
-                                href="#"
-                                className="text-gray-700 hover:text-red-600 px-3 py-2 rounded-md text-sm font-medium flex items-center"
-                            >
-                                Creativity & Design
-                                <svg className="h-4 w-4 ml-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" /></svg>
-                            </a>
-                            <a
-                                href="#"
-                                className="text-gray-700 hover:text-red-600 px-3 py-2 rounded-md text-sm font-medium flex items-center"
-                            >
-                                PDF & E-signatures
-                                <svg className="h-4 w-4 ml-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" /></svg>
-                            </a>
-                            <a
-                                href="#"
-                                className="text-gray-700 hover:text-red-600 px-3 py-2 rounded-md text-sm font-medium flex items-center"
-                            >
-                                Marketing & Commerce
-                                <svg className="h-4 w-4 ml-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" /></svg>
-                            </a>
-                            <a
-                                href="#"
-                                className="text-gray-700 hover:text-red-600 px-3 py-2 rounded-md text-sm font-medium flex items-center"
-                            >
-                                Help & Support
-                                <svg className="h-4 w-4 ml-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" /></svg>
-                            </a>
+                            {["Creativity & Design", "PDF & E-signatures", "Marketing & Commerce", "Help & Support"].map((item) => (
+                                <a
+                                    key={item}
+                                    href="#"
+                                    className="text-gray-700 hover:text-red-600 px-3 py-2 rounded-md text-sm font-medium flex items-center"
+                                >
+                                    {item}
+                                    <svg className="h-4 w-4 ml-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
+                                    </svg>
+                                </a>
+                            ))}
                         </div>
                     </div>
                     <div className="flex items-center">
-                        <button>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="w-5 h-5">
-                                <circle cx="25" cy="25" r="10" fill="black" />
-                                <circle cx="50" cy="25" r="10" fill="black" />
-                                <circle cx="75" cy="25" r="10" fill="black" />
-                                <circle cx="25" cy="50" r="10" fill="black" />
-                                <circle cx="50" cy="50" r="10" fill="black" />
-                                <circle cx="75" cy="50" r="10" fill="black" />
-                                <circle cx="25" cy="75" r="10" fill="black" />
-                                <circle cx="50" cy="75" r="10" fill="black" />
-                                <circle cx="75" cy="75" r="10" fill="black" />
-                            </svg>
-                        </button>
+                        <div className="dropdown-container relative">
+                            <button
+                                onClick={toggleDropdown}
+                                aria-label="Apps Menu"
+                                aria-expanded={isDropdownOpen}
+                                className="focus:outline-none"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" className="w-5 h-5">
+                                    {Array.from({ length: 9 }).map((_, index) => (
+                                        <circle key={index} cx={(index % 3) * 25 + 25} cy={Math.floor(index / 3) * 25 + 25} r="10" fill="black" />
+                                    ))}
+                                </svg>
+                            </button>
+                            {isDropdownOpen && (
+                                <div className="absolute right-0 mt-2 w-64 bg-white shadow-lg rounded-lg p-4 z-50">
+                                    <h3 className="text-gray-700 font-semibold mb-3">Web Apps</h3>
+                                    <div className="grid grid-cols-3 gap-4">
+                                        {[
+                                            { icon: AdobeExpressIcon, name: 'Adobe Express' },
+                                            { icon: AcrobatIcon, name: 'Acrobat' },
+                                            { icon: PhotoshopIcon, name: 'Photoshop' },
+                                            { icon: LightroomIcon, name: 'Lightroom' },
+                                            { icon: AcrobatSignIcon, name: 'Acrobat Sign' },
+                                            { icon: FontsIcon, name: 'Fonts' },
+                                            { icon: StockIcon, name: 'Stock' },
+                                            { icon: ExperienceCloudIcon, name: 'Experience Cloud' },
+                                        ].map(({ icon, name }) => (
+                                            <div key={name} className="flex flex-col items-center">
+                                                <img src={icon} alt={name} className="h-10 w-10" />
+                                                <span className="text-sm text-gray-700 mt-1">{name}</span>
+                                            </div>
+                                        ))}
+                                    </div>
+                                    <div className="border-t mt-3 pt-3 flex justify-between items-center bg-gray-100">
+                                        <a href="#" className="text-gray-800 hover:text-red-600 text-sm font-medium transition duration-200">
+                                            Adobe.com
+                                        </a>
+                                        <a href="#" className="text-gray-800 hover:text-red-600 text-sm font-medium transition duration-200">
+                                            All apps
+                                        </a>
+                                    </div>
+
+                                </div>
+                            )}
+                        </div>
                         <button
                             className="text-gray-700 m-3 hover:text-red-600 px-3 py-2 border border-black rounded-md text-sm font-medium"
                         >
                             Sign In
                         </button>
-                        {/* Hamburger Menu Button */}
                         <div className="md:hidden ml-2">
                             <button
                                 onClick={() => setIsOpen(!isOpen)}
@@ -101,35 +141,18 @@ function Navbar() {
                     </div>
                 </div>
             </div>
-
-            {/* Mobile Menu */}
             {isOpen && (
                 <div className="md:hidden">
                     <div className="px-2 pt-2 pb-3 space-y-1">
-                        <a
-                            href="#"
-                            className="text-gray-700 hover:text-red-600 block px-3 py-2 rounded-md text-base font-medium"
-                        >
-                            Creativity & Design
-                        </a>
-                        <a
-                            href="#"
-                            className="text-gray-700 hover:text-red-600 block px-3 py-2 rounded-md text-base font-medium"
-                        >
-                            PDF & E-signatures
-                        </a>
-                        <a
-                            href="#"
-                            className="text-gray-700 hover:text-red-600 block px-3 py-2 rounded-md text-base font-medium"
-                        >
-                            Marketing & Commerce
-                        </a>
-                        <a
-                            href="#"
-                            className="text-gray-700 hover:text-red-600 block px-3 py-2 rounded-md text-base font-medium"
-                        >
-                            Help & Support
-                        </a>
+                        {["Creativity & Design", "PDF & E-signatures", "Marketing & Commerce", "Help & Support"].map((item) => (
+                            <a
+                                key={item}
+                                href="#"
+                                className="text-gray-700 hover:text-red-600 block px-3 py-2 rounded-md text-base font-medium"
+                            >
+                                {item}
+                            </a>
+                        ))}
                     </div>
                 </div>
             )}
